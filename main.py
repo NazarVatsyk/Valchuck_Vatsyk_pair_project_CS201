0890cdcf15984b4d2e46dabd7edeba91b26bbc42
ValchukOleksand <oleksandr.valchuk.25@kse.org.ua>
2026-07-15 13:01:45 +0300

    created sma function to calculate simple moving average

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ start_date = datetime(2025,7,15)
 end_date = datetime(2026,7,15)
 df_eth = yf.download('ETH-USD', start_date, end_date, interval='1d')
 
+def sma(data, period):
+    df = data.copy()
+    df[f"SMA_{period}"] = df['Close'].rolling(period).mean()
+    return df
+
+
+
