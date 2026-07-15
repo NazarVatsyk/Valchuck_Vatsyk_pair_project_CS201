369ff8afc3d2f3e6e0856eb097c04a591b049205
ValchukOleksand <oleksandr.valchuk.25@kse.org.ua>
2026-07-15 12:16:59 +0300

    downloaded dataframe from yahoofinance

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+from datetime import datetime
+start_date = datetime(2025,7,15)
+end_date = datetime(2026,7,15)
+df_eth = yf.download('ETH-USD', start_date, end_date, interval='1d')
 
